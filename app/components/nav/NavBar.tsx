f8f6a28a9bd32a9e8c33cb6c845d70625aa69db0
francis omondi <francisomondi17@gmail.com>
2023-09-04 07:10:42 +0300

    Navbar pattern set

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import Container from '../container'
+import Link from 'next/link'
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({subsets: ['latin'],weight: ['400']})
 
 type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-    <div>NavBar</div>
+    <div className='sticky top-0 w-full bg-slate-200 z-30 shadow-sm'>
+      <div className='py-4 border-b-[1px]'>
+        <Container>
+          <div className='flex items-center justify-between gap-3 md:gap-0'>
+            <Link href={'/'}>
+              Ry-Interior
+            </Link>
+            <div className={`${'hidden md:block'}`}>Search</div>
+            <div className='flex items-center gap-8 md:gap-12'>
+              <div >CartCount</div>
+              <div>UserMenu</div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
   )
 }
 
