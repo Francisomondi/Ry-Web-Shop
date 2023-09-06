ee7c07ff579e70d4f3c9ea8baba4ded45938fa9a
francis omondi <francisomondi17@gmail.com>
2023-09-06 09:47:17 +0300

    add to cart button,,custom button to reuse

--- app/product/[productId]/ProductDetails.tsx
+++ app/product/[productId]/ProductDetails.tsx
@@ -1,11 +1,28 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { Rating } from "@mui/material"
+import Button from '@/app/components/Button'
 
 type productDetailsProps = {
     product:any
 }
 
+export type CartProductType={
+    id: string
+    name: string
+    description: string
+    category: string
+    brand: string
+    selectedImage: SelectedImgType 
+    quantity: number
+    price: number
+}
+
+export type SelectedImgType ={
+    color: string
+    colorCode: string
+    image: string
+}
 const Horizontal= ()=>{
     return (
         <hr className='w-[30%] my-2'/>
@@ -13,10 +30,21 @@ const Horizontal= ()=>{
 }
 
 const ProductDetails = ({product}: productDetailsProps) => {
-    const productRating= product.reviews.reduce((acc:number,item:any)=>
-  item.rating + acc,0)/product.reviews.length
+    const [cartProduct, setCartProduct] = useState<CartProductType>({
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        category: product.category,
+        brand: product.brand,
+        selectedImage: {...product.images[0]},
+        quantity: 1,
+        price: product.price,
+    })
 
-  return (
+    const productRating= product.reviews.reduce((acc:number,item:any)=>
+    item.rating + acc,0)/product.reviews.length
+
+return (
     <div className='mt-8 grid grid-cols-1 md:grid-cols-2 gap-12'>
         <div>Images</div>
         <div className='flex flex-col gap-1 text-slate-500 text-sm' >
@@ -42,10 +70,14 @@ const ProductDetails = ({product}: productDetailsProps) => {
             <Horizontal/>
             <div>Quality</div>
             <Horizontal/>
-            <div>Add to cart</div>
+            <div>
+                <Button 
+                label='Add to Cart'
+                onClick={()=>{}}/>
+            </div>
         </div>
     </div>
-  )
+)
 }
 
 export default ProductDetails
