93ef58be297b699c82bf7d033bb1c32ec58e9988
francis omondi <francisomondi17@gmail.com>
2023-09-14 05:42:11 +0300

    cart user interface

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import Container from '../components/Container'
+import CartClient from './CartClient'
 
 type Props = {}
 
 const Cart = (props: Props) => {
   return (
-    <div>Cart page</div>
+    <div className='pt-8'>
+      <Container>
+        <CartClient/>
+      </Container>
+    </div>
   )
 }
 
